735fd2e2db2f6ea95fb731c23ee9d954b890d98e
judsonpaiva <judson.paiva@hotmail.com>
2022-05-10 06:10:22 +0100

    Initial commit - Layout

---
+++ app/src/main/java/com/example/ementa_de_restaurante/ResumoPedidoActivity.java
@@ -0,0 +1,14 @@
+package com.example.ementa_de_restaurante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResumoPedidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pedido);
+    }
+}
